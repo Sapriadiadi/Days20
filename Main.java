ef67eeaa1a783b8f38f90a893c70d0c55b797ba4
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-28 23:52:47 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package days20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    if (20 > 18) {
+System.out.println("20 lebih besar dari 18"); 
+}
+}
+}
+    
